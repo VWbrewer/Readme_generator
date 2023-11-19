bcc8137c92d48a30a3b5210346817274b15f3b4c
cbrewer <144485839+VWbrewer@users.noreply.github.com>
2023-11-19 12:52:28 -0600

    fixing the README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {  
-  if (!license) {
+  if (!license=== 'None') {
   return ``;
 } else {
   return `[![${license} license](https://img.shields.io/badge/License-${license}-blue.svg)](${renderLicenseLink(license)})`;
@@ -14,7 +14,7 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (!license) {
+  if (!license === 'None') {
     return ``;
   } else if (license === 'MIT') {
     return `https://lbesson.mit-license.org/`;
@@ -29,7 +29,7 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (!license) {
+  if (!license === 'None') {
     return ``;
   } else {
     return `## License
